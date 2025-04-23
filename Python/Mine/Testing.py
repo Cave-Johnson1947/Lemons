300b77b51066efc7295815408fad5522704748bc
Jackson Peplau <jacksonmars95@gmail.com>
2025-04-22 20:38:26 -0400

    Refactor Testing.py to remove approximation logic and implement division check

--- Python/Mine/Testing.py
+++ Python/Mine/Testing.py
@@ -1,8 +1,7 @@
-approx_value = 1 + 0.9 + 0.09 + 0.009 + 0.0009 + 0.00009 + 0.000009
+numerator = int(input("Enter a numerator: "))
+denominator = int(input("Enter denominator: "))
 
-print("Approximated value:", approx_value)
-print("Is it equal to 2?", approx_value == 2)
-
-# Let's show the difference between the two
-difference = 2 - approx_value
-print("Difference from 2:", difference)
+while int(numerator / denominator) * denominator == numerator:
+    print("Divides evenly!")
+else:
+    print("Doesn't divide evenly.")
